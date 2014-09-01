6b354415b218f32ecfc034271791eff4d37696ca
Edward Ruzek <edadiddles@hotmail.com>
2014-09-01 16:05:55 -0700

    calls test member function from queue class

--- test.cpp
+++ test.cpp
@@ -14,15 +14,7 @@ int main(int argc, char* argv[])
 {
 	Queue q;
 
-	q.enqueue(0);
-	q.enqueue(1);
-	q.enqueue(2);
-
-	for(int i = 0; i < 3; i++)
-	{
-		std::cout << q.dequeue() << " ";
-	}
-	std::cout << "\n";
+	q.test();
 
 
 	return 0;
